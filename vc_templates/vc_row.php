9e8db55bd3b6c6ca96fb9bbb3dd1e9a58b50f73e
Ugyen C. Lama <86464524+ugyenchheda@users.noreply.github.com>
2023-03-23 00:08:25 +0200

    Update vc_row.php

--- vc_templates/vc_row.php
+++ vc_templates/vc_row.php
@@ -3,29 +3,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die( '-1' );
 }
 
-/**
- * Shortcode attributes
- * @var $atts
- * @var $el_class
- * @var $full_width
- * @var $full_height
- * @var $equal_height
- * @var $columns_placement
- * @var $content_placement
- * @var $parallax
- * @var $parallax_image
- * @var $css
- * @var $el_id
- * @var $video_bg
- * @var $video_bg_url
- * @var $video_bg_parallax
- * @var $parallax_speed_bg
- * @var $parallax_speed_video
- * @var $content - shortcode content
- * @var $css_animation
- * Shortcode class
- * @var $this WPBakeryShortCode_VC_Row
- */
 $el_class = $full_height = $parallax_speed_bg = $parallax_speed_video = $full_width = $equal_height = $flex_row = $columns_placement = $content_placement = $parallax = $parallax_image = $css = $el_id = $video_bg = $video_bg_url = $video_bg_parallax = $css_animation = '';
 $disable_element = '';
 $output = $after_output = '';
